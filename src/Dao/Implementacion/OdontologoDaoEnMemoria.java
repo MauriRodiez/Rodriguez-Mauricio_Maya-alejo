0bdb054cec49639661a493f1b316630ba6a7d4d3
MauriRodiez <c.maurodriguez.a@gmail.com>
2024-02-27 22:05:36 -0300

    Actualizando proyecto

--- src/Dao/Implementacion/OdontologoDaoEnMemoria.java
+++ src/Dao/Implementacion/OdontologoDaoEnMemoria.java
@@ -2,10 +2,12 @@ package Dao.Implementacion;
 
 import Dao.IDao;
 import Model.Odontologo;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class OdontologoDaoEnMemoria implements IDao<Odontologo> {
+
     @Override
     public Odontologo guardar(Odontologo odontologo) {
         return null;
--- src/Dao/Implementacion/OdontologoDaoH2.java
+++ src/Dao/Implementacion/OdontologoDaoH2.java
@@ -1,18 +1,91 @@
 package Dao.Implementacion;
 
+import DB.Database;
 import Dao.IDao;
 import Model.Odontologo;
+import org.apache.log4j.Logger;
+import org.h2.command.Prepared;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao<Odontologo> {
+
+    private static final Logger LOGGER =  Logger.getLogger(OdontologoDaoH2.class);
+    private static final String SQL_INSERT_ODONTOLOGO = "INSERT INTO ODONTOLOGOS (MATRICULA, NOMBRE, APELLIDO) VALUES (?,?,?)";
+    private static final String SQL_SELECT = "SELECT * FROM ODONTOLOGOS";
+    Connection connection = null;
     @Override
     public Odontologo guardar(Odontologo odontologo) {
-        return null;
+
+        try {
+            connection = Database.getConnection();
+
+            PreparedStatement psInsert = connection.prepareStatement(SQL_INSERT_ODONTOLOGO, Statement.RETURN_GENERATED_KEYS);
+            psInsert.setString(1, odontologo.getMatricula());
+            psInsert.setString(2, odontologo.getNombre());
+            psInsert.setString(3, odontologo.getApellido());
+
+            psInsert.execute();
+
+            ResultSet rsInsert = psInsert.getGeneratedKeys();
+            while(rsInsert.next()){
+                odontologo.setId((long) rsInsert.getInt(1));
+            }
+
+        } catch (Exception e) {
+            LOGGER.error("Se produjo un error y no es posible cargar los datos " + e.getMessage());
+        } finally {
+            try {
+                connection.close();
+                LOGGER.info("Se cerro la conexion!");
+            } catch (SQLException e) {
+                LOGGER.error("No se pudo cerrar la conexion " + e.getMessage());
+            }
+        }
+
+        LOGGER.info("Se ha creado un registro para odontologo " + odontologo);
+
+        return odontologo;
     }
 
     @Override
     public List<Odontologo> listarOdontologos() {
-        return null;
+
+        List<Odontologo> listadoOdontologos = new ArrayList<>();
+        Odontologo odontologo = null;
+
+        try {
+            connection = Database.getConnection();
+
+            PreparedStatement psListar = connection.prepareStatement(SQL_SELECT);
+
+            ResultSet rsListar = psListar.executeQuery();
+
+            while (rsListar.next()){
+                odontologo = new Odontologo(
+                        rsListar.getLong(1),
+                        rsListar.getString(2),
+                        rsListar.getString(3),
+                        rsListar.getString(4)
+                );
+                listadoOdontologos.add(odontologo);
+            }
+
+        } catch (Exception e) {
+            LOGGER.error("Se produjo un error y no es posible cargar los datos " + e.getMessage());
+        } finally {
+            try {
+                connection.close();
+                LOGGER.info("Se cerro la conexion!");
+            } catch (SQLException e) {
+                LOGGER.error("No se pudo cerrar la conexion " + e.getMessage());
+            }
+        }
+
+        LOGGER.info("Se listan todos los odontologos" + listadoOdontologos);
+
+        return listadoOdontologos;
     }
 }
