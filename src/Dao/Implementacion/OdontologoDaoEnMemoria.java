dc9db1910366a0258cfb813adbbd66520f91a51d
alergio <alejosmaya8@gmail.com>
2024-02-27 22:10:49 -0300

    subo odontologoenmemoria

--- src/Dao/Implementacion/OdontologoDaoEnMemoria.java
+++ src/Dao/Implementacion/OdontologoDaoEnMemoria.java
@@ -2,17 +2,29 @@ package Dao.Implementacion;
 
 import Dao.IDao;
 import Model.Odontologo;
+import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoEnMemoria implements IDao<Odontologo> {
+
+    Logger logger = Logger.getLogger(OdontologoDaoEnMemoria.class);
+    List <Odontologo> odontologosList = new ArrayList<>();
+
+    public OdontologoDaoEnMemoria() {
+    }
+
     @Override
     public Odontologo guardar(Odontologo odontologo) {
-        return null;
+        odontologosList.add(odontologo);
+        logger.info("Odontologo guardado " + odontologo);
+        return odontologo;
     }
 
     @Override
     public List<Odontologo> listarOdontologos() {
-        return null;
+        logger.info("La lista de odontologos es: " + odontologosList);
+        return odontologosList;
     }
 }
